da71f68fb0a342112f9cd4d3c033e1191a372bc2
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-01-05 19:40:44 -0800

    Remove Coolline special-casing

--- lib/pry/repl.rb
+++ lib/pry/repl.rb
@@ -124,12 +124,7 @@ class Pry
     def read_line(current_prompt)
       handle_read_errors do
         handle_eof do
-          if defined? Coolline and input.is_a? Coolline
-            input.completion_proc = proc do |cool|
-              completions = @pry.complete cool.completed_word
-              completions.compact
-            end
-          elsif input.respond_to? :completion_proc=
+          if input.respond_to? :completion_proc=
             input.completion_proc = proc do |input|
               @pry.complete input
             end
@@ -137,14 +132,10 @@ class Pry
 
           if input == Readline
             input.readline(current_prompt, false) # false since we'll add it manually
-          elsif defined? Coolline and input.is_a? Coolline
+          elsif input.method(:readline).arity == 1
             input.readline(current_prompt)
           else
-            if input.method(:readline).arity == 1
-              input.readline(current_prompt)
-            else
-              input.readline
-            end
+            input.readline
           end
         end
       end
